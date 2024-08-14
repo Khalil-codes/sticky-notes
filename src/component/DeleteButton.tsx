a78eec2b66faeb61844a3c2454e5150419cbd1c5
Khalil <khalilpati55@gmail.com>
2024-08-14 15:24:59 +0530

    Delete Note and invalidate fetch query

--- src/component/DeleteButton.tsx
+++ src/component/DeleteButton.tsx
@@ -1,14 +1,41 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Trash } from "lucide-react";
+import { RawNote } from "../utils/types";
+import type { Models } from "appwrite";
+import { db } from "../lib/databases";
 
 type Props = {
   id: string;
 };
 
 const DeleteButton = ({ id }: Props) => {
+  const client = useQueryClient();
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["notes", { id: id }, "delete"],
+    onSuccess: () => {
+      const notes = client.getQueryData<Models.DocumentList<RawNote>>([
+        "notes",
+      ]);
+
+      if (!notes) return;
+      const updatedNotes = notes.documents.filter((note) => note.$id !== id);
+      client.setQueryData(["notes"], {
+        documents: updatedNotes,
+        total: updatedNotes.length,
+      });
+    },
+    mutationFn: async (id: string) => {
+      await db.notes.delete(id);
+    },
+  });
+
   return (
     <button
+      disabled={isPending}
+      className="disabled:opacity-50"
       onClick={() => {
         console.log("delete", id);
+        mutate(id);
       }}>
       <Trash size={20} />
     </button>
